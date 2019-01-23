cdac2ae589a34bb40e7fa88573f0100adefb73fb
ehalsey <ehalsey@a-cto.com>
2019-01-22 23:29:40 -0800

    initial load

--- TestSendSMTPMessage/Program.cs
+++ TestSendSMTPMessage/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mail;
 
 namespace TestSendSMTPMessage
 {
@@ -6,7 +7,36 @@ namespace TestSendSMTPMessage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SendMsg();
+            Console.ReadKey();
+
+        }
+
+        private static void SendMsg()
+        {
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress("ehalsey@a-cto.com", "Eric"));
+            msg.From = new MailAddress("ehalseyhb@actodev2.onmicrosoft.com", "Eric");
+            msg.Subject = "This is a Test Mail";
+            msg.Body = "This is a test message using Exchange OnLine";
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("ehalseyhb@actodev2.onmicrosoft.com", "");
+            client.Port = 587; // You can use Port 25 if 587 is blocked (mine is!)
+            client.Host = "smtp.office365.com";
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            try
+            {
+                client.Send(msg);
+                Console.WriteLine("Message Sent Succesfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
